fab263162bb041cb5b109335249b2ce1482e3805
naveen-perera <naveen.perera@syscolabs.com>
2019-01-15 19:56:07 +0530

    POST,UPDATE,DELETE

--- src/main/java/io/javacourse/api/topic/TopicService.java
+++ src/main/java/io/javacourse/api/topic/TopicService.java
@@ -1,21 +1,42 @@
 package io.javacourse.api.topic;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-   private List<Topic> topics = Arrays.asList(
+   private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("Spring","Spring FW","Java EE FW"),
                 new Topic("React-Redux","Javascript FW","Frontend dev"),
                 new Topic("Mongo","No-SQL","For schema-less DBs")
-                );
+   ) );
    public List<Topic> getAllTopics(){
        return  topics;
    }
 
     public Topic getTopic(String id){
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void  addTopic(Topic topic){
+       topics.add(topic);
+    }
+
+    public void updateThisTopic(String id , Topic topic){
+       for (int i=0; i<topics.size();i++){
+           Topic t = topics.get(i);
+           if(t.getId().equals(id)){
+               topics.set(i, topic);
+               return;
+           }
+       }
+    }
+
+    public void deleteThisTopic(String id){
+       topics.removeIf(t ->t.getId().equals(id));
+
     }
 }
